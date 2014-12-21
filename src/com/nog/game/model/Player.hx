690e37214386dda1b00aab1125ccb8884e0bc34f
Colapsydo <Colapsydo@gmail.com>
2014-12-21 23:02:43 +0100

    New AI study data 0.5-1.0

--- src/com/nog/game/model/Player.hx
+++ src/com/nog/game/model/Player.hx
@@ -33,10 +33,10 @@ class Player
 	var _power:Int;
 	
 	//AI CALIBRATION
-	static var _ag:Float = .5;
-	static var _dg:Float = .2;
-	static var _ab:Float = .2;
-	static var _db:Float = .1;
+	static var _ag:Float = .2;
+	static var _dg:Float = .3;
+	static var _ab:Float = .1;
+	static var _db:Float = .4;
 	
 	static var _calIndex:Int = 0;
 	static var _calList:Vector<Float> = Vector.ofArray([]);
